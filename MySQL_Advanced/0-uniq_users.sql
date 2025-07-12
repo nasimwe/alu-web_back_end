12dc357b7c40e3b49c5ccbdc2213a7666faff15c
nasimwe <144112258+nasimwe@users.noreply.github.com>
2025-07-12 13:49:45 +0200

    Delete MySQL_Advanced directory

--- MySQL_Advanced/0-uniq_users.sql
+++
@@ -1,7 +0,0 @@
--- create user table if it doesn't exist
-CREATE TABLE IF NOT EXISTS users (
-    id int auto_increment not null,
-    email varchar(255) unique not null,
-    name varchar(255),
-    primary key (id)
-)
--- MySQL_Advanced/2-fans.sql
+++
@@ -1,2 +0,0 @@
--- get number of fans per country
-SELECT origin, SUM(fans) AS nb_fans from metal_bands group by origin order by nb_fans DESC;
